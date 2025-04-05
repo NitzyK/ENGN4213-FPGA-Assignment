f01ea573d4426c7a4cdc6092811dbb87dd9d73fb
Nitin Kesarapu <u7486968@anu.edu.au>
2025-04-05 13:00:30 +1100

    added clockdividerHB

---
+++ keyboardTo7SD/vivado/keyboardTo7SD.srcs/sources_1/imports/new/clockDividerHB.v
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04.03.2025 14:14:54
+// Design Name: 
+// Module Name: clockDividerHB
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clockDividerHB #(parameter integer THRESHOLD = 50_000_000)(
+input wire clk,
+input wire enable,
+input wire reset,
+output reg dividedClk ,
+output wire beat
+    );
+    
+    reg [31:0] counter;
+    
+    always @(posedge clk) begin
+        if (reset==1'b1 || counter >= THRESHOLD-1) begin
+            counter <= 32'd0;
+        end else if (enable == 1'b1) begin
+            counter <= counter + 1'b1;
+        end
+    end
+    
+    always @(posedge clk) begin
+        if (reset == 1'b1) begin
+            dividedClk <= 1'b0;
+        end else if (counter >= THRESHOLD-1) begin
+            dividedClk <= ~dividedClk;
+        end
+    end
+    
+    assign beat=(counter==THRESHOLD-1)&(dividedClk);
+endmodule
