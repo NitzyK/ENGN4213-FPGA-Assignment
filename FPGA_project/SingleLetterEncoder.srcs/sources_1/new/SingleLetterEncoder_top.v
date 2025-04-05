b69272b1099a0c3a2c63d93fb5143c77ea3705ba
Jesse Stevens <u7487477@anu.edu.au>
2025-04-05 13:34:10 +1100

    copied FSM

--- FPGA_project/SingleLetterEncoder.srcs/sources_1/new/SingleLetterEncoder_top.v
+++ FPGA_project/SingleLetterEncoder.srcs/sources_1/new/SingleLetterEncoder_top.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05.04.2025 12:25:10
-// Design Name: 
-// Module Name: SingleLetterEncoder_top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module SingleLetterEncoder_top(
 
